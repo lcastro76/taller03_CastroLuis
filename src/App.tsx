cbed2984482ce6cab70a703df264962e8d936810
lcastro76 <lcastro76@cuc.edu.co>
2024-09-23 09:22:05 -0500

    m1

---
+++ src/App.tsx
@@ -0,0 +1,16 @@
+import "./App.css";
+import miLogo from "./assets/img/nvidiaLogo.png";
+
+function App() {
+  return (
+    <>
+      <div>
+        <img src={miLogo} alt="Logo de PlayStation" />
+        <h1>Hola Mundo!!</h1>
+        <p>Taller 03, Luis Castro</p>
+      </div>
+    </>
+  );
+}
+
+export default App;
